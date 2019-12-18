4c8378902a3fcfb766c94e5357070ba0de92b4ce
Maricaya <915270549@qq.com>
2019-12-18 11:26:28 +0800

    feat: common elements in object list

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -29,8 +29,12 @@ public class Main {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Person)) return false;
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Person)) {
+                return false;
+            }
             Person person = (Person) o;
             return Objects.equals(name, person.name);
         }
