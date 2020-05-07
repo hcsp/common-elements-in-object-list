2fefd672402c7dfec61ef51f06a02fdf867c012e
DobbyKim <1175088275@qq.com>
2020-05-07 22:46:54 +0800

    完善commonElementsIn方法

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -6,11 +6,10 @@ public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
     public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
         Set<Person> res = new HashSet<>();
-        for (Person p : a) {
-            if (b.contains(p)) {
-                res.add(p);
-            }
-        }
+
+        res.addAll(a);
+        res.retainAll(b);
+
         return res;
     }
 
