7c51cd76b7c79ff40592052f18d99b7ffd6d78d7
DobbyKim <1175088275@qq.com>
2020-05-07 22:30:33 +0800

    完善commonElementsIn方法

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,22 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> res = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        for(Person p : a){
+            set.add(p.getName());
+        }
+        for(Person p : b){
+            if(set.contains(p.getName())){
+                res.add(p);
+            }
+        }
+        return res;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -23,6 +33,7 @@ public class Main {
         public void setName(String name) {
             this.name = name;
         }
+
     }
 
     public static void main(String[] args) {
