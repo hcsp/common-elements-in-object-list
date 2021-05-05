546a90fcd55f181d42880dce7f313654dff754bd
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-05-05 17:06:05 +0800

    第2次提交

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
