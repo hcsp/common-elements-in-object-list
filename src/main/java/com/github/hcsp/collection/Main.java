c7c12702dad979e64d94f03ebfd4da97df1b60f3
dexter_git <runfenglin@qq.com>
2023-11-29 17:31:51 +0800

    学习best solution， 采用hashSet.retainAll()取交集

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -5,18 +5,10 @@ import java.util.*;
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
     public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
-        Set<Person> uncommonSet = new HashSet<>(a);
-        for (Person p :
-                b) {
-            uncommonSet.remove(p);
-        }
-        Set<Person> commonSet = new HashSet<>(a);
-        for (Person p :
-                uncommonSet) {
-            commonSet.remove(p);
-        }
-
-        return commonSet;
+        Set<Person> setA = new HashSet<>(a);
+        Set<Person> setB = new HashSet<>(b);
+        setA.retainAll(setB);
+        return setA;
     }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
